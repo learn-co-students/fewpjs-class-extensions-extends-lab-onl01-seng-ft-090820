dfe1ae4f6ed4b638d2ada829b8b45e681442dd16
JohnZonneveld <jjczon@gmail.com>
2020-12-04 17:26:07 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Your code here
+class Polygon {
+    constructor(arr){
+        this.arr = arr
+    }
+
+    get countSides() {
+        return this.arr.length
+    }
+
+    get perimeter() {
+        let sum = 0
+        // for (let i = 0; i < this.arr.length; i++) {
+        //     sum = sum + this.arr[i]
+        // }
+        // return(sum)
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        return this.arr.reduce(reducer)
+    }
+
+}
+
+class Triangle extends Polygon {
+    
+    get isValid() {
+        if (this.countSides === 3) {
+            let a = this.arr[0]
+            let b = this.arr[1]
+            let c = this.arr[2]
+            return ((a + b > c) && (b + c > a) && (c + a > b))
+        } else {
+            return false
+        }
+    }
+}
+    class Square extends Polygon {
+    
+        get isValid() {
+            if (this.countSides === 4) {
+                
+                return ((this.arr[0] === this.arr[1]) && (this.arr[1] === this.arr[2]) && (this.arr[2] === this.arr[3]))
+            } else {
+                return false
+            }
+        }
+
+        get area() {
+            if (this.isValid) {
+                return (this.arr[0] * this.arr[1])
+            }
+        }
+
+       
+}
