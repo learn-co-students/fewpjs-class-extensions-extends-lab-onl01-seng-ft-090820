93a6bce90dadd03694a8d4997cb38d7e688ed79f
doosemavis <moosedavis2011@gmail.com>
2021-01-01 14:27:50 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+class Polygon {
+    constructor(array) {
+        this.array = array;
+    }
+
+    get coundSides() {
+
+    }
+
+    get permieter() {
+
+    }
+}
+
+class Triangle extends Polygon {
+
+    get isValid() {
+
+    }
+}
+
+class Square extends Polygon {
+
+    get isValid() {
+
+    }
+
+    get area() {
+        
+    }
+}
+
