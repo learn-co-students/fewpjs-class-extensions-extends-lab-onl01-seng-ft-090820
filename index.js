c62093305d3a9aa5754a1cfbec1b73e9546ee091
doosemavis <moosedavis2011@gmail.com>
2021-01-01 14:33:53 -0600

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 class Polygon {
     constructor(array) {
-        this.array = array;
+        this.array = array
     }
-
-    get coundSides() {
-
+    get countSides() {
+        return this.array.length
     }
-
-    get permieter() {
-
+    get perimeter() {
+        return this.array.reduce((a, b) => a + b, 0)
     }
 }
 
 class Triangle extends Polygon {
-
     get isValid() {
-
+        return (this.countSides === 3 && 
+            this.array[0] + this.array[1] > this.array[2] &&
+            this.array[1] + this.array[2] > this.array[0] &&
+            this.array[2] + this.array[0] > this.array[1])
     }
 }
 
 class Square extends Polygon {
-
     get isValid() {
-
+        return this.array[0] === this.array[1] && this.array[1] === this.array[2] && this.array[2] === this.array[3] 
     }
-
     get area() {
-        
+        return this.isValid ? this.array[0] ** 2 : "not a square!"
     }
 }
-
